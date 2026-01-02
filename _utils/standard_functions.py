64c7bd14032372872c6a1f7e2f9eb356e587c4f7
tzm <thezenmaestro@gmail.com>
2026-01-01 22:54:14 -0500

    working website summarizer

--- _utils/standard_functions.py
+++ _utils/standard_functions.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 from IPython.display import display, update_display
 from IPython.display import Markdown
 
-load_dotenv(override=True)
+def load_env():
+    load_dotenv(override=True)
+    
+    open_api_key = os.getenv('OPENAI_API_KEY')
+
+    if not open_api_key:
+        print("No API key was found!")
+    elif not open_api_key.startswith("sk-proj-"):
+        print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key. Open AI API must start with sk-proj-")
+    else:
+        print("API key found and looks valid!")
+
+    OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+
+
+
 
 def stream_api_response(model, messages):   
     api_key = os.getenv('OPENAI_API_KEY')
