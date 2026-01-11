c1c25ed635820f56f5a4c7fb616dba0aaae8d83d
tzm <thezenmaestro@gmail.com>
2026-01-10 21:31:43 -0500

    added tokenizer demo

--- _utils/standard_functions.py
+++ _utils/standard_functions.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from IPython.display import display, update_display
 from IPython.display import Markdown
+from huggingface_hub import login
 
 def load_env():
     load_dotenv(override=True)
@@ -17,6 +18,12 @@ def load_env():
     groq_api_key        = os.getenv('GROQ_API_KEY')
     grok_api_key        = os.getenv('GROK_API_KEY')
     openrouter_api_key  = os.getenv('OPENROUTER_API_KEY')
+    hf_token            = os.getenv('HF_TOKEN')
+
+    if hf_token and hf_token.startswith("hf_"):
+        print("HF key looks good so far")    
+    else:
+        print("HF key is not set - please set HF_TOKEN in your .env file")
 
     if openai_api_key:
         print(f"OpenAI API Key exists and begins with {openai_api_key[:8]}")
